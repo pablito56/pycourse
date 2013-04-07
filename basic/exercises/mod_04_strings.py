54a8e9c393caed9b68b527a3ffac3eb49f3f9ebe
Sergi Sorribas <sergisj@tid.es>
2013-04-07 19:14:38 +0200

    exercise to TestCase structure

--- basic/exercises/mod_04_strings.py
+++ basic/exercises/mod_04_strings.py
@@ -3,16 +3,16 @@
 
 u'''
 Copyright (c) Telefonica I+D. All rights reserved.
-Description: 
+Description:
 '''
-
+import unittest
 
 
 def sum_chars_text(text):
     '''given a text str, add all chars by its code
         tip:
             >>> c = 'c'
-            >>> ord(c) 
+            >>> ord(c)
             99
     '''
     pass
@@ -46,25 +46,28 @@ def join_str_unicode(*items):
     pass
 
 
-def test_sum_chars_text():
-    '''implement sum_chars_text'''
-    text = 'In the face of ambiguity, refuse the temptation to guess.'
-    assert sum_chars_text(text) == 5307
+class ModStringTestCase(unittest.TestCase):
 
+    def test_sum_chars_text():
+        '''implement sum_chars_text'''
+        text = 'In the face of ambiguity, refuse the temptation to guess.'
+        assert sum_chars_text(text) == 5307
 
-def test_reverse_text_by_word():
-    '''implement reverse_text_by_word'''
-    text = "rules. the break to enough special aren't cases Special"
-    assert reverse_text_by_word(text) == "Special cases aren't special enough to break the rules."
+    def test_reverse_text_by_word():
+        '''implement reverse_text_by_word'''
+        text = "rules. the break to enough special aren't cases Special"
+        assert reverse_text_by_word(text) == "Special cases aren't special enough to break the rules."
 
+    def test_remove_identation_multiline_string():
+        '''implement remove_identation_multiline_string'''
+        text = """ Complex is better than complicated.
+                    Flat is better than nested."""
+        assert remove_identation_multiline_string(text) == 'Complex is better than complicated. '\
+                                                           'Flat is better than nested.'
 
-def test_remove_identation_multiline_string():
-    '''implement remove_identation_multiline_string'''
-    text = """ Complex is better than complicated.
-                Flat is better than nested."""
-    assert remove_identation_multiline_string(text) == 'Complex is better than complicated. Flat is better than nested.'
+    def test_join_str_and_unicode():
+        items = ('el señor', u'de los anillos')
+        assert join_str_unicode(items) == 'el se\xc3\xb1or de los anillos'
 
-
-def test_join_str_and_unicode():
-    items = ('el señor', u'de los anillos')
-    assert join_str_unicode(items) == 'el se\xc3\xb1or de los anillos'
+if __name__ == '__main__':
+    unittest.main()
