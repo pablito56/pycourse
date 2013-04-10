73f1d0d8bd479eeab522b5be4f16dc62ee8791e6
Sergi Sorribas <sergisj@tid.es>
2013-04-10 11:27:48 +0200

    added introductory exercises

---
+++ basic/exercises/mod_00_intro.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+u'''
+Copyright (c) Telefonica I+D. All rights reserved.
+Description: introductory exercises from pythonmonk.com
+'''
+
+
+def minimum(a, b):
+    """Computes minimum of given 2 numbers.
+    
+        >>> minimum(2, 3)
+        2
+        >>> minimum(8, 5)
+        5
+        """
+    # your code here
+
+
+def maximum3(a, b, c):
+    """Computes maximum of given three numbers.
+    
+    	>>> maximum3(2, 3, 5)
+        5
+    	>>> maximum3(12, 3, 5)
+        12
+    	>>> maximum3(2, 13, 5)
+        13
+    """
+    # your code here
+
+
+def istrcmp(s1, s2):
+    """Compare given two strings for equality, ignoring the case.
+    
+    	>>> istrcmp("python", "Python")
+        True
+    	>>> istrcmp("latex", "LaTeX")
+        True
+    	>>> istrcmp("foo", "Bar")
+        False
+    """
+    # your code here
+
+
+def unique(values):
+    """Finds all unique elements of a list.
+
+        >>> unique([])
+        []
+        >>> unique([1, 2, 1])
+        [1, 2]
+        >>> unique([1, 2, 1, 3, 4, 2])
+        [1, 2, 3, 4]
+    """
+    # your code here
+
+
+def isort(names):
+    """Sorts a list of strings ignoring the case.
+    
+        >>> isort(['BOB', 'ALICE', 'dave', 'charlie'])
+        ['ALICE', 'BOB', 'charlie', 'dave']
+    """
+    # your code here
+
